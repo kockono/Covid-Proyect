9d59835306788853dc0649d6a210f39e7b957ea2
Joshua Komaba <joshuakomaba@gmail.com>
2021-01-30 22:07:51 -0700

    Config de las apis

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,43 @@
-let frontend;
-let backend;
+const express = require("express")
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+var app = express();
+
+var puerto = 3000
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://covidUser:<password>@cluster0.z6sl4.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.use(cors())
+app.use(bodyParser.json( {limit:'100mb'} ))
+app.use(bodyParser.urlencoded(
+    {
+        extended: true,
+        limit: '100mb'
+    }
+))
+
+app.all('*', function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
+
+app.get('/api/inicio', function(req, res){
+    res.send({
+        msg: 'Funciona'
+    })
+})
+
+app.listen(puerto, function () {
+    console.log("Api corriendo en el puerto " + puerto);
+})
