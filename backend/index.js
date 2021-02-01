b974c78ff6cf1b046d21ddb3255725249282874f
kockono <kockonoop@gmail.com>
2021-01-31 20:02:10 -0700

    merge

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 const express = require("express")
 const cors = require("cors");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser"); El body parser komaba se dejo de usar  en la version de Express 4.16.0 o mayor ya lo trae incluido el express
 
-var app = express();
+let app = express();
 
-var puerto = 3000
+let puerto = 3000
 
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://covidUser:<password>@cluster0.z6sl4.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
@@ -17,8 +18,10 @@ client.connect(err => {
 });
 
 app.use(cors())
-app.use(bodyParser.json( {limit:'100mb'} ))
-app.use(bodyParser.urlencoded(
+// app.use(bodyParser.json( {limit:'100mb'} ))
+app.use(express.json( {limit:'100mb'} ))
+app.use(express.urlencoded(
+    // app.use(bodyParser.urlencoded(
     {
         extended: true,
         limit: '100mb'
