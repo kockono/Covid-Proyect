bc0f95a90a7444654d85d39ca5d7a30ec437eed0
kockono <kockonoop@gmail.com>
2021-02-07 18:24:44 -0700

    backend encuesta

--- backend/src/encuesta.routes.js
+++ backend/src/encuesta.routes.js
@@ -2,16 +2,75 @@ const { Router } = require('express');
 const Encuestas = require('./models/encuesta');
 const router = Router();
 
-router.post('/respuesta', async(req, res) => {
-    const { pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, encuestado } = req.body
-    const encuesta = new Encuestas({pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, encuestado})
-    await encuesta.save()
-    res.status(200).json()
-})
 
 router.get('/respuestas', async(req, res) => {
     var respuestas = await Encuestas.find()
     return res.status(200).json(respuestas)
+})
+
+router.post('/preguntas/:id', async(req, res) => {
+
+
+    let question =  {pregunta1, pregunta2, pregunta3, pregunta4, pregunta5} = req.body;
+    let ID = req.params.id;
+    let questionOriginal;
+
+    Encuestas.findById(ID, (err , doc) => {
+        questionOriginal = doc;
+        console.log(questionOriginal)
+        console.log(question)
+        if(question.pregunta1.frecuente === 1) {
+            questionOriginal.pregunta1.pocoFrecuente = questionOriginal.pregunta1.Frecuente + 1;
+        }
+        if(question.pregunta1.pocoFrecuente === 1){
+            questionOriginal.pregunta1.pocoFrecuente = questionOriginal.pregunta1.pocoFrecuente + 1;
+        }
+        if(question.pregunta1.nunca === 1){
+            questionOriginal.pregunta1.nunca = questionOriginal.pregunta1.nunca +1
+        }
+        if(question.pregunta2.si === 1) {
+            questionOriginal.pregunta2.si = questionOriginal.pregunta2.si + 1;
+        }
+        if(question.pregunta2.no === 1) {
+            questionOriginal.pregunta2.no = questionOriginal.pregunta2.no + 1;
+        }
+        if(question.pregunta3.si === 1) {
+            questionOriginal.pregunta3.si = questionOriginal.pregunta3.si + 1;
+        }
+        if(question.pregunta3.no === 1) {
+            questionOriginal.pregunta3.no = questionOriginal.pregunta3.no + 1;
+        }
+        if(question.pregunta4.muySeguido === 1) {
+            questionOriginal.pregunta4.mu = questionOriginal.pregunta4.mu + 1;
+        }
+        if(question.pregunta4.seguido === 1) {
+            questionOriginal.pregunta4.se = questquestionOriginalon.pregunta4.se + 1;
+        }
+        if(question.pregunta4.noTanSeguido === 1) {
+            questionOriginal.pregunta4.no = questionOriginal.pregunta4.no + 1;
+        }
+        if(question.pregunta4.nadaSeguido === 1) {
+            questionOriginal.pregunta4.na = questionOriginal.pregunta4.na + 1;
+        }
+        if(question.pregunta5.si === 1) {
+            questionOriginal.pregunta5.si = questionOriginal.pregunta5.si + 1;
+        }
+        if(question.pregunta5.no === 1) {
+            questionOriginal.pregunta5.no = questionOriginal.pregunta5.no + 1;
+        }
+    
+        Encuestas.findByIdAndUpdate(ID, {$set: questionOriginal}, {new:true},(err, doc) => {
+                if(!err) {res.send(doc)}
+                  else { console.log(`Error en encontrar la empresa: `+ JSON.stringify(err, undefined, 2));}
+        
+            })
+    
+
+    })
+
+    const encuesta = new Preguntas({questionOriginal})
+    await encuesta.save()
+    
 })
 
 module.exports = router;
--- backend/src/models/encuesta.js
+++ backend/src/models/encuesta.js
@@ -1,12 +1,30 @@
 const { Schema, model } = require('mongoose');
 
 const pregSchema = new Schema ({
-    pregunta1: String,
-    pregunta2: String,
-    pregunta3: String,
-    pregunta4: String,
-    pregunta5: String,
-    encuestado: String
+    pregunta1: {
+        frecuente: Number,
+        pocoFrecuente: Number,
+        nunca: Number,
+    },
+    pregunta2: {
+        si: Number,
+        no: Number,
+    },
+    pregunta3: {
+        si: Number,
+        no: Number,
+    },
+    pregunta4: {
+        muySeguido: Number,
+        seguido: Number,
+        noTanSeguido: Number,
+        nadaSeguido: Number,
+    },
+    pregunta5: {
+        si: Number,
+        no: Number,
+    }
+
 },{
     timestamps:true
 });
