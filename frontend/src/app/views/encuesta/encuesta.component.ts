cc0615753ad623fe7e592aec5243f87785aa6f25
kockono <kockonoop@gmail.com>
2021-02-11 12:05:10 -0700

    encuestas

--- frontend/src/app/views/encuesta/encuesta.component.ts
+++ frontend/src/app/views/encuesta/encuesta.component.ts
@@ -11,21 +11,49 @@ export class EncuestaComponent implements OnInit {
   constructor(private authService:AuthService) { }
 
   Encuestas = {
-    pregunta1:'',
-    pregunta2:'',
-    pregunta3:'',
-    pregunta4:'',
-    pregunta5:'',
-    encuestado:'',
+      pregunta1: {
+        frecuente: 0,
+        pocoFrecuente: 0,
+        nunca: 0
+    },
+    pregunta2: {
+        si: 0,
+        no: 0
+    },
+    pregunta3: {
+        si: 0,
+        no: 0
+    },
+    pregunta4: {
+        muySeguido: 0,
+        seguido: 0,
+        noTanSeguido: 0,
+        nadaSeguido: 0
+    },
+    pregunta5: {
+        si: 0,
+        no: 0
+    }
+  }
+
+  EncuestasComposicion ={
+    pregunta1: 0,
+    pregunta2: 0,
+    pregunta3: 0,
+    pregunta4: 0,
+    pregunta5: 0,
+
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
-    this.authService.encuestas(this.Encuestas).subscribe(res => {
+    
+    console.log(this.EncuestasComposicion)
+    // this.authService.encuestas(this.Encuestas).subscribe(res => {
 
-    })
+    // })
   }
 
 }
